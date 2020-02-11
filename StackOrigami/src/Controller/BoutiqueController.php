85f0a6c5ecb476cd80664615a6066c59ed460e91
leorousseldev <leo.roussel@hotmail.fr>
2020-02-11 11:44:46 +0100

    'hey'

--- StackOrigami/src/Controller/BoutiqueController.php
+++ StackOrigami/src/Controller/BoutiqueController.php
@@ -7,6 +7,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+
 
 class BoutiqueController extends AbstractController {
 
@@ -25,6 +29,22 @@ class BoutiqueController extends AbstractController {
         $form->handleRequest($request);
         return $this->render('boutique/contact.html.twig', [
                     'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about_us")
+     */
+    public function about() {
+        return $this->render('boutique/about_us.html.twig');
+    }
+
+    /**
+     * @Route("/catalog", name="catalog")
+     */
+    public function catalog(ProductRepository $productRepository) {
+        return $this->render('product/index.html.twig', [
+                    'products' => $productRepository->findAll(),
         ]);
     }
 
