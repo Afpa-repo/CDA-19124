5e64c03eed5b00e338382d3ad3ea0a6a2cc2a09c
leorousseldev <leo.roussel@hotmail.fr>
2020-02-19 12:33:17 +0100

    pb composer

--- StackOrigami/src/Controller/SecurityController.php
+++ StackOrigami/src/Controller/SecurityController.php
@@ -7,8 +7,11 @@ use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class SecurityController extends AbstractController
 {
@@ -42,9 +45,18 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(){
-        return $this->render('security/login.html.twig');
-    }
+        public function login(AuthenticationUtils $authenticationUtils): Response
+        {
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+
+            return $this->render('security/login.html.twig', [
+                'last_username'=>$lastUsername,
+                'error'=>$error
+            ]);
+        }
 
     /**
      * @Route("/logout", name="security_logout")
