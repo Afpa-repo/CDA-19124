461a809e841d0ecd128850aa3da4d9da89ae006e
80010-91-15 <80010-91-15@exchange.ad.afpanet>
2020-02-21 12:24:15 +0100

    Ajout de la fonction commande Pensez a doctrine:schema:update

--- StackOrigami/src/Controller/CartController.php
+++ StackOrigami/src/Controller/CartController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
-use App\Entity\Product;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
+use App\Entity\ProductCategory;
+use App\Entity\Orders;
+use App\Entity\Users;
+use App\Entity\OrderDetails;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class CartController extends AbstractController
 {
@@ -25,27 +28,30 @@ class CartController extends AbstractController
           'product' => $productRepository->find($id),
           'quantity' => $quantity
         ];
-        // code...
       }
 
       $total = 0;
-      foreach ($panierWithData as $item) {
-          
+      foreach ($panierWithData as $item) {          
           $totalItem = $item['product']->getPrice()*$item['quantity'];
-          $total += $totalItem;// code...
+          $total += $totalItem;
           $count += $item['quantity']; 
       }
       //ajoute un champs Fcount a la variable session avec le nombre de produit total dans le panier 
       $session->set('Fcount',$count);
-      
-      
-
+      $session->set('FullCart',$panierWithData);
+      //dd($session);
+      // Si Le panier est valider alors vide le panier .
+      if($session->get('Validate',"false")=="true"){
+            unset($panierWithData);
+            $session->remove('Validate');
+            $session->remove('panier');
+            $session->set('Fcount',0);
+            return $this->redirectToRoute("profil");
+        }
         return  $this->render('cart/index.html.twig', [
           'items' => $panierWithData,
           'total' => $total,
-          'Cquantity' => $count,
-          
-                
+          'Cquantity' => $count 
         ]) ;
     }
     /**
@@ -75,7 +81,7 @@ class CartController extends AbstractController
     * @Route("/cart/remove/{id}", name="cart_remove")
     */
     public function remove($id, SessionInterface $session){
-        $_POST['nb_product'];
+      
       $panier= $session->get('panier',[]);
       if(!empty($panier[$id])){        
           if($_POST['nb_product']<=0){              
@@ -94,4 +100,38 @@ class CartController extends AbstractController
 
         return $this->redirectToRoute("cart_index");
     }
-}
+    
+    /**
+    * @Route("/cart/validate", name="cart_validate")
+    */
+    
+    // Validation du panier.
+    public function validate(SessionInterface $session, UserInterface $user) {         
+        $order = new Orders();  //Crée un objet Orders
+        $order->setUsersID($user);    //récupère les valeurs
+        $order->setDate(new \DateTime());
+        $order->setStatus("En cours de traitement"); // a changer dans l'interface employés
+        /*** Rajouter les valeurs ***/
+        $order->setBill('jeej');
+        $order->setDeliveryForm('jeej');
+        
+         $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($order);
+            $entityManager->flush();
+            
+            foreach ($session->get('FullCart',[])as $produit){  //pour chaque produit
+                $orderDetails = new OrderDetails(); //on crée un objet OrderDetails
+                $orderDetails->setOrders($order);//récupere la valeur de order
+                $orderDetails->setQuantity($produit['quantity']); 
+                $orderDetails->setProduct($produit['product']);
+            $entityManager->merge($orderDetails);
+            $entityManager->flush();    //On le rajoute dans la base de donnée
+            }
+            
+            $session->set('Validate',"true");
+        return $this->redirectToRoute("cart_index");    //redirige vers le profil
+    }
+    
+    
+    
+      }
