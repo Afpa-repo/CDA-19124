9a84d66bc0ec2293bfa534dc4914767f517fd2ca
leorousseldev <leo.roussel@hotmail.fr>
2020-03-02 11:34:16 +0100

    d

---
+++ StackOrigami/public/js/basket.js
@@ -0,0 +1,9 @@
+var btn_basket = document.getElementById('add_basket');
+var basket = document.getElementById('basket');
+btn_basket.addEventListener('click', add);
+
+function add() {
+
+   basket.style.fontSize = '30px';
+
+}
